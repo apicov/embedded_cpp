fb6cbfcc12facdc3c8b05d648c63502a2e457ebd
pico <a.pico@posteo.de>
2026-05-22 17:01:56 +0200

    convert c toggle led to c++ class

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,10 +1,28 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main() {
-    DDRB |= (1 << PB5);   // pin 13 on Uno = PB5
-    for(;;) {
-        PORTB ^= (1 << PB5);
-        _delay_ms(500);
+class ScopedLED{
+  public:
+    ScopedLED(volatile uint8_t &ddr, volatile uint8_t &port, uint8_t pin): ddr_(ddr), port_(port), pin_(pin){
+      ddr_ |= (1 << pin_);
+      port_ |= (1 << pin_);
     }
+
+    ~ScopedLED(){
+      port_ &= ~(1 << pin_);
+    }
+
+  private:
+    volatile uint8_t &ddr_;
+    volatile uint8_t &port_;
+    uint8_t pin_;
+};
+
+
+
+
+int main() {
+  ScopedLED led(DDRB, PORTB, PB5);
+  _delay_ms(5000);
+  return 0;
 }
